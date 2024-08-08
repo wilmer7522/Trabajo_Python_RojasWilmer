0d6b8e2c0aca201f1d1f68ab9bee025731179922
wilmer <wilmer7522@gmail.com>
2024-08-08 09:44:30 -0500

    :sparkles: creado json de compras, ventas y visualizacion de ventas y compras

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # Trabajo_Python_RojasWilmer
+
+# PanCamp
+Este es un sistema de gestion una panaderia en la cual se usan archivos *.json* para llevar un registro de las ventas realizadas, las compras a proveedores y uno para los productos disponibles.
+
+En este programa podras acceder a un menu en el cual puedes realizar una venta, registrar una comprar a un proveedor y ver informes de ventas y compras realizadas
+## Estructura
+
+![Alt text](<Captura de Pantalla 2024-08-07 a la(s) 11.30.50 p. m..png>)
+
+
+## Tecnologias usadas
+
+![Python](https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=blue)
+![json](https://img.shields.io/badge/json-5E5C5C?style=for-the-badge&logo=json&logoColor=white)
+
+## Funciones
+|Archivo|Funcion|
+|--|--|
+|**compras.json**|Archivo para almacenar el historial de compra a los proveedores|
+|**tienda.py**|Archivo para ejecutar el programa|
+|**productos.json**|Archivo en el cual se encuentran los productos disponibles con su nombre y precio|
+|ventas.json|Archivo en el cual se guarda el registro de las ventas realizadas|
+
+## Instalación y Uso
+**Debes tener instalado visual studio code, python y git en su ultima version**
+- Para ello debes clonar el directorio del siguiente link de git hub https://github.com/wilmer7522/Trabajo_Python_RojasWilmer.git
+
+- Abre la terminal de git bash y allí escribe el siguiente comando:     
+
+``` bash 
+git clone https://github.com/wilmer7522/Trabajo_Python_RojasWilmer.git
+```
+- Abre visual studio code e instalas la extencion de python
+
+
+![image](https://github.com/user-attachments/assets/371fc5fb-ec7a-4c0e-a1ae-af9c21f1b78c)
+
+
+
+- Abres una nueva terminal de git dando siguiendo las siguientes instrucciones:
+![Alt text](<Captura de Pantalla 2024-08-08 a la(s) 9.30.10 a. m..png>)
+
+
+![Alt text](<Captura de Pantalla 2024-08-08 a la(s) 9.31.49 a. m..png>)
+
+
+![Alt text](<Captura de Pantalla 2024-08-08 a la(s) 9.27.38 a. m..png>)
+
+
+
+
+![Alt text](<Captura de Pantalla 2024-08-08 a la(s) 9.38.29 a. m..png>)
+
+
+
+- Y finalmente ejecutas el siguiente comando en git bash
+```bash
+python3 tienda.py
+```
+
+## Créditos
+
+Desarrollado por: *Wilmer Rojas*
--- tienda.py
+++ tienda.py
@@ -1,4 +1,7 @@
+from os import system
+from datetime import date
 import json
+
 
 def abrir():
     mijson=[]
@@ -6,22 +9,97 @@ def abrir():
         mijson = json.load(openfile)
     return mijson
 
+def abrirVentas():
+    venta=[]
+    with open('ventas.json', 'r', encoding='utf=8') as openfile:
+        venta = json.load(openfile)
+    return venta
+
+def abrirCompras():
+    compra=[]
+    with open('compras.json', 'r', encoding='utf=8') as openfile:
+        compra = json.load(openfile)
+    return compra
+
 def guardar(data):
     with open('productos.json', 'w', encoding='utf=8') as outfile:
         json.dump(data,outfile)
 
+def guardarVentas(dataVenta):
+    with open('ventas.json', 'w', encoding='utf=8') as outfile:
+        json.dump(dataVenta,outfile)
+
+def guardarCompras(dataCompra):
+    with open('compras.json', 'w', encoding='utf=8') as outfile:
+        json.dump(dataCompra,outfile)
+
 #inicio
-booleano = True
+        
+general = True
 
-while booleano == True:
+while general == True:
+
+    booleano = True
+
+    while booleano == True:
+        print("---------------------------------")
+        print("     Bienvenido a PanCamp        ")
+        print("---------------------------------") 
     
-
-    seleccion = int(input("elegir la opcion: " ))
+        try:
+            seleccion = int(input("Ver Productos 1: \nVentas 2: \nCompras 3:  \nVer Ventas Realizadas 4: \nVer Compras Realizadas 5: \nIngrese opcion:"))
+            
+            booleano = False
+        except ValueError:
+            input("ingrese un valor valido")
+            system("cls")
 
     if seleccion == 1:
         general=abrir()
-        for i in general["Panaderia"]:
-            print(i["Nombre"])
+        for i in general[0]["Panaderia"]:
+            print(f" ID" ,i["id"], i["Nombre"], "$", i["Precio"])
+
+    if seleccion== 2:
+        nuevaVenta = {}
+        fecha= date.today()
+        nuevaVenta["fechaVenta"] = fecha.isoformat()
+        nuevaVenta["nombreCliente"] = input("Nombre del cliente: ")
+        nuevaVenta["nombreEmpleado"] = input("nombre del empleado: ")
+        nuevaVenta["producto"] = (input("nombre del producto: "))
+   
+
+        venta = abrirVentas()
+        venta[0]["ventas"].append(nuevaVenta)
+        guardarVentas(venta)
+        print("venta registrada exitosamente.")
+
+
+    if seleccion== 3:
+        nuevaCompra = {}
+        fecha= date.today()
+        nuevaCompra["fechaCompra"] = fecha.isoformat()
+        nuevaCompra["nombreProveedor"] = input("Nombre del Proveedor: ")
+        nuevaCompra["contactoProveedor"] = input("Contacto de Proveedor: ")
+        nuevaCompra["producto"] = (input("nombre del producto: "))
+        nuevaCompra["cantidad"] = int(input("Cantidad Comprada: "))
+        nuevaCompra["Precio"] = int(input("Precio del Producto: "))
+   
+
+        compra = abrirCompras()
+        compra[0]["compras"].append(nuevaCompra)
+        guardarCompras(compra)
+        print("Compra registrada exitosamente.")
+
+    if seleccion == 4:
+        sell=abrirVentas()
+        for i in sell[0]["ventas"]:
+            print(f"Fecha de Venta:",i["fechaVenta"],"\n","Cliente:",  i["nombreCliente"],"\n","Empleado:", i["nombreEmpleado"],"\n","Producto:", i["producto"])
+                
+    if seleccion == 5:
+        shop=abrirCompras()
+        for i in shop[0]["compras"]:
+            print(f"Fecha de Compra:",i["fechaCompra"],"\n","Proveedor:",  i["nombreProveedor"],"\n","Contacto:", i["contactoProveedor"],"\n","Producto:", i["producto"], "Cantidad:" ,i["cantidad"], "Precio:", i["Precio"])
+                
 
 
 
